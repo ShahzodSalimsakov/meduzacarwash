ae51ccd2f64cafb4027bdd3d0d424beea8e0fc09
Dave93 <yul.davron.93@gmail.com>
2022-11-27 12:44:41 +0500

    try to server via node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 import node from "@astrojs/node";
@@ -12,6 +11,6 @@ export default defineConfig({
   integrations: [tailwind(), astroI18next(), react()],
   output: "server",
   adapter: node({
-mode: 'standalone'
-})
+    mode: "standalone",
+  }),
 });
